c262d0d0646ebc5a910eebec5559169a0df996c6
mystère <contact@myst.re>
2023-11-05 17:17:12 +0100

    Update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,15 @@ import { Logo } from "../islands/Logo.tsx";
 
 export function Header() {
   return (
-    <div class="bg-black flex gap-3 px-6 py-3 text-white justify-center">
-      <Logo/>
-      <a className={"hover:text-black hover:bg-white"} href={"/blog"}>blog</a>
-      <a className={"hover:text-black hover:bg-white"} href={"/music"}>music</a>
-      <a className={"hover:text-black hover:bg-white"} href={"/contact"}>contact</a>
-      <a className={"hover:text-black hover:bg-white"} href={"/projects"}>projects</a>
+    <div className={"flex flex-col justify-center max-w-screen-md mx-auto mb-3"}>
+      <div class="bg-black flex gap-3 px-6 py-3 text-white justify-center">
+        <Logo/>
+        <a className={"hover:text-black hover:bg-white"} href={"/blog"}>blog</a>
+        <a className={"hover:text-black hover:bg-white"} href={"/music"}>music</a>
+        <a className={"hover:text-black hover:bg-white"} href={"/contact"}>contact</a>
+        <a className={"hover:text-black hover:bg-white"} href={"/projects"}>projects</a>
+      </div>
+      <hr className={"w-full"}/>
     </div>
   );
 }
